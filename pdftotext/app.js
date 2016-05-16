ffb648056b7f5c8f2f27d1c35667ff2fac67f545
mervynhky <mervyn93hky@gmail.com>
2016-05-16 11:45:52 +0800

    renderPage completed

--- pdftotext/app.js
+++ pdftotext/app.js
@@ -5,22 +5,25 @@ var textLayer = document.createElement('div');
 textLayer.className = 'textLayer';
 
 
-function App(curPage) {
+function App() {
     var self = this;
+    this.pdf = null;
     this.complete = 0;
-    console.log("curPage in App is : " + curPage);
 
-    this.pdfToText = function (data) {
+    this.reset = function(){
+        var nodes = document.querySelectorAll(".textLayer > div");
+        var parent = document.getElementsByClassName("textLayer")[0];
+
+        for (var j = 0; j < nodes.length; j++) {
+            console.log(nodes[j]);
+            parent.removeChild(nodes[j]);
+        }
+    }
+    this.renderPage = function (pageNumber) {
         var div = document.getElementById('viewer');
+        var page = this.pdf.getPage(pageNumber);
 
-        // render the first pages
-        var pdf = new PDFJS.PDFDoc(data);
-        //var total = pdf.numPages;
-
-        //for (i = 1; i <= total; i++){
-        var page = pdf.getPage(curPage);
-
-        canvas.id = 'page' + curPage;
+        canvas.id = 'page' + pageNumber;
         canvas.mozOpaque = true;
         div.appendChild(canvas);
 
@@ -35,28 +38,28 @@ function App(curPage) {
 
 
         self.setMessage("Rendering...");
-
         document.body.appendChild(textLayer);
 
         page.startRendering(context, function () {
-                //if (++self.complete == total){
-                if (++self.complete == curPage) {
-                    self.setMessage("Finished rendering. Extracting text...");
+                self.setMessage("Finished rendering. Extracting text...");
 
-                    window.setTimeout(function () {
-                        var layers = [];
-                        var nodes = document.querySelectorAll(".textLayer > div");
-                        for (var j = 0; j < nodes.length; j++) {
-                            layers.push(nodes[j].textContent + "\n");
-                        }
-                        self.sendOutput(layers.join("\n"));
-                        //console.log("TEXT LAYERS MERGED: " + layers.join("\n"));
-                        self.setMessage("( ͡° ͜ʖ ͡°)  Done!");
-                    }, 1000);
-                }
+                window.setTimeout(function () {
+                    var layers = [];
+                    var nodes = document.querySelectorAll(".textLayer > div");
+                    for (var j = 0; j < nodes.length; j++) {
+                        layers.push(nodes[j].textContent + "\n");
+                    }
+                    self.sendOutput(layers.join("\n"));
+                    self.setMessage("( ͡° ͜ʖ ͡°)  Done!");
+
+                }, 1000);
             },
             textLayer);
-        //}
+    };
+
+    this.pdfToText = function (data) {
+        // render the first pages
+        this.pdf = new PDFJS.PDFDoc(data);
     };
 
     this.receiveInput = function (event) {
@@ -83,58 +86,61 @@ function App(curPage) {
 //TOP FUNCTION CALLED TO RENDER FIRST PAGE
 //BOTTOM FUNCTION CALLED FOR PAGING AFTER FIRST PAGE
 
-function Apple(curPage) {
-    var self = this;
-    console.log("curPage in App is : " + curPage);
-
-    this.pdfToText = function (data) {
-        var div = document.getElementById('viewer');
-
-        // render the first pages
-        var pdf = new PDFJS.PDFDoc(data);
-        var page = pdf.getPage(curPage);
-
-        self.setMessage("Rendering...");
-
-        document.body.removeChild(textLayer);
-        document.body.appendChild(textLayer);
-
-        page.startRendering(context, function () {
-                self.setMessage("Finished rendering. Extracting text...");
-
-                window.setTimeout(function () {
-                    var layers = [];
-                    var nodes = document.querySelectorAll(".textLayer > div");
-                    for (var j = 0; j < nodes.length; j++) {
-                        layers.push(nodes[j].textContent + "\n");
-                    }
-                    self.sendOutput(layers.join("\n"));
-                    self.setMessage("( ͡° ͜ʖ ͡°) Done!");
-                }, 1000);
-            },
-            textLayer);
-    };
-
-    this.receiveInput = function (event) {
-        if (event.source != parent) return;
-        if (!event.data.byteLength) return alert("The PDF data needs to be an ArrayBuffer");
-        self.setMessage("Received data");
-        self.pdfToText(event.data);
-    }
-
-    this.sendOutput = function (text) {
-        var recipient = parent.postMessage ? parent : (parent.document.postMessage ? parent.document : undefined);
-        recipient.postMessage(text, "*");
-    };
-
-    this.setMessage = function (text) {
-        document.getElementById("message").textContent = text;
-    }
-
-    window.addEventListener("message", self.receiveInput, true);
-    self.setMessage("Ready");
-    self.sendOutput("ready");
-}
+//function Apple(curPage) {
+//    var self = this;
+//    console.log("curPage in App is : " + curPage);
+//
+//    this.pdfToText = function (data) {
+//        var div = document.getElementById('viewer');
+//
+//        // render the first pages
+//        var pdf = new PDFJS.PDFDoc(data);
+//        delete pdf.pageCache;
+//        pdf = new PDFJS.PDFDoc(data);
+//        var page = pdf.getPage(curPage);
+//
+//        self.setMessage("Rendering...");
+//
+//        //document.body.removeChild(textLayer);
+//        document.body.appendChild(textLayer);
+//
+//        page.startRendering(context, function () {
+//                self.setMessage("Finished rendering. Extracting text...");
+//
+//                window.setTimeout(function () {
+//                    var layers = [];
+//                    var nodes = document.querySelectorAll(".textLayer > div");
+//                    for (var j = 0; j < nodes.length; j++) {
+//                        layers.push(nodes[j].textContent + "\n");
+//                    }
+//                    self.sendOutput(layers.join("\n"));
+//                    console.log(layers.join("\n"));
+//                    self.setMessage("( ͡° ͜ʖ ͡°) Done!");
+//                }, 1000);
+//            },
+//            textLayer);
+//    };
+//
+//    this.receiveInput = function (event) {
+//        if (event.source != parent) return;
+//        if (!event.data.byteLength) return alert("The PDF data needs to be an ArrayBuffer");
+//        self.setMessage("Received data");
+//        self.pdfToText(event.data);
+//    }
+//
+//    this.sendOutput = function (text) {
+//        var recipient = parent.postMessage ? parent : (parent.document.postMessage ? parent.document : undefined);
+//        recipient.postMessage(text, "*");
+//    };
+//
+//    this.setMessage = function (text) {
+//        document.getElementById("message").textContent = text;
+//    }
+//
+//    window.addEventListener("message", self.receiveInput, true);
+//    self.setMessage("Ready");
+//    self.sendOutput("ready");
+//}
 
 //function AppTwo(curPage) {
 //    var self = this;
